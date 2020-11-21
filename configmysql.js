3dfbec261fd3ae7e8dc01d0566722e340123f9f9
rizalazky <rizalaazky26@gmail.com>
2020-11-21 22:09:57 +0700

    first commit

---
+++ configmysql.js
@@ -0,0 +1,21 @@
+var mysql = require('mysql')
+
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'db_danaku'
+})
+
+connection.connect((err)=>{
+    if(err){
+        throw err;
+        
+    }else{
+        console.log('Koneksi DB Berhasil')
+    }
+})
+
+
+module.exports=connection
---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const conn=require('./configmysql')
+
+
+app.get('/',(req,res)=>{
+    conn.query('SELECT * FROM tbl_user',(err,result)=>{
+        if(err){
+            throw err
+        }else{
+            res.json(result)
+        }
+    })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
