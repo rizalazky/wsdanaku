8905781052dbe87089c0eefaae93c741ea641317
rizalazky <rizalaazky26@gmail.com>
2020-11-22 23:53:40 +0700

    config mysql

--- configmysql.js
+++ configmysql.js
@@ -2,10 +2,10 @@ var mysql = require('mysql')
 
 
 var connection = mysql.createConnection({
-  host: 'localhost:3306',
+  host: 'localhost',
   user: 'svnvbkdl_pemalicomal2019',
   password: 'pemalicomal',
-  database: 'svnvbkdl_dbapemasi'
+  database: 'svnvbkdl_db_danaku'
 })
 
 // $server   = "localhost:3306";
