14eee5c336180051c7c0bcbc393f32c0128f4a6a
rizalazky <rizalaazky26@gmail.com>
2020-11-22 22:21:20 +0700

    register

--- app.js
+++ app.js
@@ -2,18 +2,42 @@ const express = require('express')
 const app = express()
 const port = 3000
 const conn=require('./configmysql')
+const cors=require('cors')
+const bodyParser=require('body-parser')
+// corrs
+app.use(cors())
 
+// body parser
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
 
 app.get('/',(req,res)=>{
-    // conn.query('SELECT * FROM tbl_user',(err,result)=>{
-    //     if(err){
-    //         throw err
-    //     }else{
-    //         res.json(result)
-    //     }
-    // })
-
-    res.send('Halloo Ari Ganteng')
+    conn.query('SELECT * FROM tbl_user',(err,result)=>{
+        if(err){
+            throw err
+        }else{
+            res.json(result)
+        }
+    })
 })
 
-app.listen()
+app.post('/register',(req,res)=>{
+    console.log(req.body)
+
+    
+    let no_telp=req.body.no_telp
+    conn.query(`INSERT INTO tbl_user (no_telp) VALUES ('${no_telp}')`,(err,result)=>{
+        if(err) throw err
+
+        res.json({
+            'status':'200',
+            'message':'Berhasil Update Data'
+        })
+    })
+})
+
+app.listen(port,()=>{
+    console.log('Runn on'+port)
+})
