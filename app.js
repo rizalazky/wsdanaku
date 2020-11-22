2cd143981adbc1169a20906944c8c07458f2f788
rizalazky <rizalaazky26@gmail.com>
2020-11-22 21:02:12 +0700

    haloo

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ const conn=require('./configmysql')
 
 
 app.get('/',(req,res)=>{
-    conn.query('SELECT * FROM tbl_user',(err,result)=>{
-        if(err){
-            throw err
-        }else{
-            res.json(result)
-        }
-    })
+    // conn.query('SELECT * FROM tbl_user',(err,result)=>{
+    //     if(err){
+    //         throw err
+    //     }else{
+    //         res.json(result)
+    //     }
+    // })
+
+    res.send('Halloo')
 })
 
 app.listen(port, () => {
